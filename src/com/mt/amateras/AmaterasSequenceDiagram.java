d689f5cea2ad52561d9ffce82a0e5608cc25aa03
wwmengtao <wwmengtao@163.com>
2016-03-30 16:18:02 +0800

    类调用栈信息采用文件形式填写
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/amateras/AmaterasSequenceDiagram.java
+++ src/com/mt/amateras/AmaterasSequenceDiagram.java
@@ -1,34 +1,38 @@
 package com.mt.amateras;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 public class AmaterasSequenceDiagram{
-	String filename="stackTrace.txt";
+	String inFileName="files/stackTraceIn.txt";
+	String outFileName="files/stackTraceOut.txt";
 	String str_start = "at ";
 	String str_end="(1.java:1)";
 	String newline = "\r\n";// \r\n即为换行  
+	String line = "";
 	public void createAmaterasSequenceDiagram(){
-		String []callInfos={
-				"KeyboardLayoutDialogFragment.getView",
-				"KeyboardLayoutDialogFragment.getView",
-				"AbsListView.obtainView",
-				"ListView.addViewBelow",
-				"ListView.scrollListItemsBy"
-		};
-        /* 写入Txt文件 */  
 		try{
-			File fileToWrite = new File(filename);
-			fileToWrite.createNewFile(); // 创建新文件  
-	        BufferedWriter out = new BufferedWriter(new FileWriter(fileToWrite));  
-			for(int i=0;i<callInfos.length;i++){
-				//dayin(str_start+callInfos[i]+str_end);
-		        out.write(str_start+callInfos[i]+str_end+newline); 
+            File fileToRead = new File(inFileName);
+			File fileToWrite = new File(outFileName);
+			if(fileToWrite.exists()){
+				fileToWrite.delete();
 			}
-	        out.flush(); // 把缓存区内容压入文件  
-	        out.close(); // 最后记得关闭文件  
-		} catch (Exception e) {  
+			fileToWrite.createNewFile(); // 创建新文件
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(fileToRead)); // 建立一个输入流对象reader  
+            BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言  
+	        BufferedWriter out = new BufferedWriter(new FileWriter(fileToWrite));  
+            while ((line = br.readLine()) != null) {// line = br.readLine(); // 一次读入一行数据  
+                out.write(str_start+line+str_end+newline); 
+                dayin(str_start+line+str_end+newline); 
+    	        out.flush(); // 把缓存区内容压入文件
+            }  
+			br.close();
+	        out.close(); // 最后记得关闭文件
+		} catch (Exception e) {
             e.printStackTrace();
         }
 	}
