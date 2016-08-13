3f79fcbeb31fa0253bd4360338ccfb2c91175126
wwmengtao <wwmengtao@163.com>
2016-08-13 12:51:18 +0800

    修改
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/array.java
+++ src/com/mt/array.java
@@ -2,13 +2,22 @@ package com.mt;
 
 
 public class array {
+	
+	public static void main(String []args){
+		new array();
+	}	
+	
+	public array(){
+		visitArray();
+	}
+	
 	public void visitArray(){
     	String [][] tags_values = {{"tag1","value1"},
 		         {"tag2","value2"},
 		         {"tag3","value3"}
 		         };
 		for(int i=0;i<tags_values.length;i++){//遍历纵向的数据
-			javaTest.dayin(tags_values[i][0]+tags_values[i][1]);
+			javaTest.dayin(tags_values[i][0]+": "+tags_values[i][1]);
 		}
 		/*
 		for(int j=0;j<tags_values[i].length;j++){//遍历横向的数据
